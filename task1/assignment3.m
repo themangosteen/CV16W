4043579e3ec22f44ad30d124663214918d631582
Patrick <patrick.wahrmann@gmail.com>
2016-11-05 17:31:57 +0100

    Assignment3: Add code and images for the additional questions in the report

--- task1/assignment3.m
+++ task1/assignment3.m
@@ -1,20 +1,51 @@
 function [] = assignment3( path_to_input_image )
 %ASSIGNMENT3  Scale-Invariant Blob Detection
 %   with scale-normalized Laplacian of Gaussians operator
+%   Use this function if you want to execute this assignment with your own
+%   image.
+calculateAssignment3( path_to_input_image, 1, 1 );
+end
+
+function [] = assignment3WithCraterImage()
+% This method demonstrates the algorithm from this assignment with the
+% bombCraters image.
+calculateAssignment3( 'Images and Functions/bombCraters.jpg', 1, 1 );
+end
+function [] = assignment3WithButterflyImage()
+% This method demonstrates the algorithm from this assignment with the
+% bombCraters image.
+calculateAssignment3( 'Images and Functions/butterfly.jpg', 1, 1 );
+end
+function [] = assignment3WithPlottingFilterResponsesOfOnePoint()
+% This method handles the task which is defined in the second bullet point
+% of assignment 3: Plotting the response of a certain point (we chose
+% 257/370) in both scales of the image.
+responseOfPBigImage = calculateAssignment3( 'Images and Functions/bombCraters.jpg', 257, 370 );
+responseOfPSmallImage = calculateAssignment3( 'Images and Functions/bomb_half.jpg', 129, 185 );
+scale = 1:size(responseOfPBigImage, 1);
+figure
+plot(scale,responseOfPBigImage, scale,responseOfPSmallImage)
+end
+
+function [filterResponseAtXY] = calculateAssignment3( path_to_input_image, px, py )
+% Calculates the scale invariant blob detection.
+% px and py are the coordinates of the point for which the filter response
+% should be returned in the variable filterResponseAtXY (needed for an
+% extra exercice).
 
 %% Standard Parameters
 sigma0 = 2;
 k = 1.25;
 levels = 10;
-threshold = 0.4; % ideal for butterfly image
-%threshold = 0.25; % ideal for bomb image
+%threshold = 0.4; % ideal for butterfly image
+threshold = 0.25; % ideal for bomb image
 %threshold = 0.3; % ideal for rain image (but still bad results)
 %threshold = 0.07; % ideal for alphabet image (but terrible)
 
 %% Preparation
 image = im2double(imread(path_to_input_image)); % load image
 if size(image, 3)==3 % color image
-  image = rgb2gray(image);  
+    image = rgb2gray(image);
 end
 scale_space = zeros(size(image,1),size(image,2),levels); % results after filtering
 sigma = sigma0; % sigma: scale (of gauss etc)
@@ -25,43 +56,46 @@ for i = 1:levels % calculate all sigmas (increase always by multiplying with con
     logFilter = fspecial('log', filterSize, sigma); % create LoG filter
     % Response to filter decreases with increasing size. We have to
     % normalize it, by multiplying with sigma^2
-    logFilter = logFilter*sigma*sigma; 
+    logFilter = logFilter*sigma*sigma;
     % Use 'same' and 'replicate' to avoid artefacts at the borders and different
     % output dimensions.
     scale_space(:,:,i) = imfilter(image, logFilter, 'same', 'replicate');
     sigma = sigma*k; % change filter size of next iteration
 end
+% For assignment3WithPlottingFilterResponsesOfOnePoint:
+filterResponseAtXY = zeros(levels,1);
+filterResponseAtXY(:) = scale_space(px,py,:);
 
 %% Non-maximum suppression
 scale_space = abs(scale_space); % only search for absolute maximums
 scale_space(scale_space<threshold) = 0; % only consider filter responses >= threshold
-maxima = zeros(size(scale_space,1), size(scale_space,2),levels);
+maxima = zeros(size(scale_space,1), size(scale_space,2),levels); % initialize array
 for i = 1:levels % check for maxima on all levels
     % compare with same level
-      maxima(:,:,i) = compareWithLevel(scale_space(:,:,i), scale_space(:,:,i), true);
-
-      % compare with level above (i+1)
+    maxima(:,:,i) = compareWithLevel(scale_space(:,:,i), scale_space(:,:,i), true);
+    
+    % compare with level above (i+1)
     if i ~= levels % there exists a level above
         maxima(:,:,i) = maxima(:,:,i) & compareWithLevel(scale_space(:,:,i), scale_space(:,:,i+1), true);
     end
     
     % compare with level below (i-1)
     if i ~= 1 % there exists a level below
-       maxima(:,:,i) = maxima(:,:,i) & compareWithLevel(scale_space(:,:,i), scale_space(:,:,i-1), true);
+        maxima(:,:,i) = maxima(:,:,i) & compareWithLevel(scale_space(:,:,i), scale_space(:,:,i-1), true);
     end
 end
 
 % now in maxima there are 1s everywhere where a maximum is.
 % find returns (linear) indices of nonzero values, ind2sub resizes them to 3
 % components
-% ex: 
+% ex:
 % maxima =
 % [0,0,0,0;
 %  0,0,1,0;
 %  0,0,0,0;...]
 % find => [7,...]
 % ind2sub => [3,2,1;...]
-[y, x, level] = ind2sub(size(maxima),find(maxima)); 
+[y, x, level] = ind2sub(size(maxima),find(maxima));
 showResults(image, x, y, level);
 end
 
@@ -85,14 +119,14 @@ filter(3,2,8) = 1;
 filter(3,3,9) = 1;
 
 if same
-   % Unshifted same level consists of zeros for comparison, so there is 
-   % always a maximum if you compare it with this level
-    filter(2,2,5) = 0; 
+    % Unshifted same level consists of zeros for comparison, so there is
+    % always a maximum if you compare it with this level
+    filter(2,2,5) = 0;
 end
 
 resultOfComparisons = ones(size(originalLevel,1), size(originalLevel,2));
 
-for i = 1:9 
+for i = 1:9
     % move level with the help of filter defined above
     movedCompareLevel = imfilter(levelToCompare, filter(:,:,i));
     % compare with this level (but only if it is still possible that it is
@@ -106,5 +140,5 @@ end
 end
 
 function [  ] = showResults( image, x, y, level )
-    show_all_circles(image, x, y, level*sqrt(2), 'r', 2.0);
+show_all_circles(image, x, y, level*sqrt(2), 'r', 1.0);
 end
