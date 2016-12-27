2d4680dd9f5f79fcc08755fb88c325cdc1fcf06a
Patrick <patrick.wahrmann@gmail.com>
2016-12-27 15:37:59 +0100

    Implement feature point matching

--- task2/assignment4/assignment4.m
+++ task2/assignment4/assignment4.m
@@ -1,4 +1,5 @@
 %% Initialisation (Reading images)
+close all; % close all open graphs
 imagename = 'campus';
 for i=1:5 % for every image
     path = strcat('img/',imagename,num2str(i),'.jpg');
@@ -6,16 +7,31 @@ for i=1:5 % for every image
 end
 
 %% A. SIFT Interest Point Detection
-image = single(zeros(size(images,2),size(images,3))); % vl_sift needs single precision
+siftDescriptors = {};
+siftFrames = {};
+image = single(zeros(size(images,2),size(images,3))); % vl_sift needs single precision 
 for i=1:5 % for every image
     image(:,:) = single(images(i,:,:)); % vl_sift needs axb as dim, not 1xaxb or 5xaxb
-    [siftFrames,siftDescriptors] = vl_sift(image);
-    % Frames are circles for descriptions of feature points
+    [siftFrames{i},siftDescriptors{i}] = vl_sift(image);
+    % Frames are featurePoints (coordinates, size, direction?)
     figure; 
     imshow(image)
-    vl_plotframe(siftFrames);
+    vl_plotframe(siftFrames{i}); % plot descriptors on top of image
 end
 
 %% B. Interest Point Matching and Image Registration
+for i=1:4 % for every image pair
+    % match feature points
+    [matches,scores] = vl_ubcmatch(siftDescriptors{i},siftDescriptors{i+1});
+    imageA = zeros(size(images,2),size(images,3)); % initialize
+    imageB = zeros(size(images,2),size(images,3)); % initialize
+    imageA(:,:) = images(i,:,:); % 1xaxb --> axb
+    imageB(:,:) = images(i+1,:,:); % 1xaxb --> axb
+    % extract the coordinates from the siftFrames matrix
+    pointsA = siftFrames{i}(1:2,matches(1,:));
+    pointsB = siftFrames{i+1}(1:2,matches(2,:));
+    % Plot the results (matched feature points)
+    match_plot(imageA, imageB, pointsA', pointsB');
+end
 
 %% C. Image Stitching
